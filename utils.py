85851711757a577c8a54ab87e6e7c35a0332ce46
Nicolasc-ER <nicolaseconomynash@gmail.com>
2023-11-24 17:54:18 -0500

    Production function-dialogflow-get-file for balancer url

--- utils.py
+++ utils.py
@@ -57,7 +57,7 @@ def generate_file_url(ambiente, file_name):
     if ambiente == "HML":
         base_url = "https://vcc.agatadata.net/function-dialogflow-get-file"
     elif ambiente == "PROD":
-        base_url = "https://us-west1-bogotatrabaja-prd.cloudfunctions.net/function-dialogflow-get-file"
+        base_url = "https://serverless.desarrolloeconomico.gov.co/function-dialogflow-get-file"
     else:
         raise ValueError("Invalid environment")
 
